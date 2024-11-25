f2a8498629f7afdbb50c3e2d91b65cdd2bc99996
Katerina Kulevtsova <66613405+binkki@users.noreply.github.com>
2024-11-25 16:34:36 +0400

    feat: add auth guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,18 +2,21 @@ import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { SignupDto } from './dto/signup.dto';
+import { Public } from './auth.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
+  @Public()
   @HttpCode(HttpStatus.CREATED)
   async signup(@Body() signupDto: SignupDto) {
     return await this.authService.signup(signupDto);
   }
 
   @Post('login')
+  @Public()
   async login(@Body() loginDto: LoginDto) {
     const token = await this.authService.login(loginDto);
     return token;
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger';
 import * as yamljs from 'yamljs';
+import { JwtService } from '@nestjs/jwt';
+import { AuthGuard } from './auth/auth.guard';
 
 import { AppModule } from './app.module';
 
@@ -8,6 +10,11 @@ const port = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const jwtService = app.get(JwtService);
+  const reflector = app.get(Reflector);
+  app.useGlobalGuards(new AuthGuard(jwtService, reflector));
+
   const document = yamljs.load('./doc/api.yaml');
   SwaggerModule.setup('doc', app, document);
   await app.listen(port);
